3de13cb139e09c0bc6253bf0d0a4b4a47fbdc8c9
maianhtra <riandlopespun0suy@hotmail.com>
2024-05-07 15:11:35 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function uniquePaths(m, n) {
-  const dp = Array.from(Array(m), () => Array(n).fill(0));
-  for (let i = 0; i < m; i++) {
-    dp[i][0] = 1;
-  }
-  for (let j = 0; j < n; j++) {
-    dp[0][j] = 1;
-  }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+const binarySearchIterative = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
   }
-  return dp[m - 1][n - 1];
-}
+  return -1;
+};
