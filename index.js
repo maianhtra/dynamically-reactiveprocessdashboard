4c1690c483dd7d61acb6fdf53e933141649c0a44
maianhtra <riandlopespun0suy@hotmail.com>
2024-05-15 12:34:31 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const currentDate = new Date();
-console.log(currentDate); // Fri 2024-04-05T09:17:00.000Z
+function combinationSum(candidates, target) {
+  const result = [];
+  backtrack(0, [], 0);
+  return result;
+  function backtrack(start, current, sum) {
+    if (sum === target) {
+      result.push([...current]);
+      return;
+    }
+    if (sum > target || start === candidates.length) return;
+    for (let i = start; i < candidates.length; i++) {
+      current.push(candidates[i]);
+      backtrack(i, current, sum + candidates[i]);
+      current.pop();
+    }
+  }
+}
